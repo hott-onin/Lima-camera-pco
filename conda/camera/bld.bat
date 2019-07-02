057150fed62ff0bf9f7db42bf75a2cf06c872b34
Samuel Debionne <samuel.debionne@esrf.fr>
2019-07-02 14:16:59 +0200

    Add /verbosity:detailed

--- conda/camera/bld.bat
+++ conda/camera/bld.bat
@@ -5,5 +5,5 @@ rem cmake -Bbuild -H. -G "%CMAKE_GENERATOR%" -DCAMERA_ENABLE_PYTHON=1 -DCAMERA_E
 cmake -Bbuild -H. -G "%CMAKE_GENERATOR%" -DCAMERA_ENABLE_PYTHON=1 -DWITH_GIT_VERSION=1 -DLIMA_ENABLE_DEBUG=0 -DCAMERA_ENABLE_TESTS=1 -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPYTHON_SITE_PACKAGES_DIR=%SP_DIR% -DCMAKE_FIND_ROOT_PATH=%LIBRARY_PREFIX%
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cmake --build build --config Release --target install
+cmake --build build --config Release --target install -- /verbosity:detailed
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
